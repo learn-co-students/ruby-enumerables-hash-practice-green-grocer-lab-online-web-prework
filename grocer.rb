339877071b1e6b1de66a1608bc785234dd2d7fd1
Patrick Harrison <jahman@bellatlantic.net>
2019-06-21 15:11:12 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -29,5 +29,24 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  # code here
+ [
+		{"AVOCADO" => {:price => 3.00, :clearance => true}},
+		{"KALE" => {:price => 3.00, :clearance => false}},
+		{"BLACK_BEANS" => {:price => 2.50, :clearance => false}},
+		{"ALMONDS" => {:price => 9.00, :clearance => false}},
+		{"TEMPEH" => {:price => 3.00, :clearance => true}},
+		{"CHEESE" => {:price => 6.50, :clearance => false}},
+		{"BEER" => {:price => 13.00, :clearance => false}},
+		{"PEANUTBUTTER" => {:price => 3.00, :clearance => true}},
+		 
+		{:item => "AVOCADO", :num => 2, :cost => 5.00},
+		{:item => "BEER", :num => 2, :cost => 20.00},
+		{:item => "CHEESE", :num => 3, :cost => 15.00}
+	def apply_clearance(cart)
+  # def generate_cart
+	[].tap do |cart|
+		rand(20).times do
+			cart.push(items.sample)
+  		end }
 end
+}
