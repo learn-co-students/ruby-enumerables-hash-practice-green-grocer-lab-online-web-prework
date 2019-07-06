e7dc2c8c1bbbc2e1fba9801c1e76ee109e10afb2
Shaun <ycorredius@gmail.com>
2019-07-06 02:05:33 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -26,22 +26,26 @@ def apply_coupons(cart, coupons)
 	while count < coupons.length
 		coupon = coupons[count][:item]
 		if cart.has_key?(coupon)
-			if coupon.match(/W\/COUPON/)
-				cart[coupon][:count] = cart[coupon][:count] + coupons[count][:num]
-				count += 1
-			end
-			else
-				cart[coupon][:count] = cart[coupon][:count] - coupons[count][:num]
-				cart[coupon + " W/COUPON"] ={
-					:price => coupons[count][:cost]/coupons[count][:num],
-					:clearance => cart[coupon][:clearance],
-					:count => coupons[count][:num]
-				}
-			count +=1
+      if coupon.match(/W\/COUPON/)
+        cart[coupon][:count] = cart[coupon][:count] * 2
+        count += 1
+        binding.pry
+      else
+        cart[coupon][:count] = cart[coupon][:count] - coupons[count][:num]
+			  cart[coupon + " W/COUPON"] ={
+				      :price => coupons[count][:cost]/coupons[count][:num],
+				      :clearance => cart[coupon][:clearance],
+				      :count => coupons[count][:num]
+				        }
+			  count +=1
+      end
+    else
+      count += 1
 		end
-	end
-	cart
+  end
+  cart
 end
+
 
 def apply_clearance(cart)
   # code here
