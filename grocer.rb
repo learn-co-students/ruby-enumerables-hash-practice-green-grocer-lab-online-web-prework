a327136f0a6a86a98c70a9c2c9ce0d2e9c98d210
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-07-09 19:42:21 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,56 @@
 def consolidate_cart(cart)
-  # code here
+  hash = {}
+  cart.each do |item_hash|
+    item_hash.each do |name, price_hash|
+      if hash[name].nil?
+        hash[name] = price_hash.merge({:count => 1})
+      else
+        hash[name][:count] += 1
+      end
+    end
+  end
+  hash
 end
-
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon|
+    if cart.keys.include? coupon[:item]
+      if cart[coupon[:item]][:count] >= coupon[:num]
+        new_name = "#{coupon[:item]} W/COUPON"
+        if cart[new_name]
+          cart[new_name][:count] += coupon[:num]
+        else 
+          cart[new_name] = {
+            count: coupon[:num],
+            price: coupon[:cost]/coupon[:num],
+            clearance: cart[coupon[:item]][:clearance]
+          }
+        end
+        cart[coupon[:item]][:count] -= coupon[:num]
+      end
+    end
+  end
+  cart
 end
-
 def apply_clearance(cart)
-  # code here
+  cart.each do |item, price_hash|
+    if price_hash[:clearance] == true
+      price_hash[:price] = (price_hash[:price] * 0.8).round(2)
+    end
+  end
+  cart
 end
 
-def checkout(cart, coupons)
-  # code here
+def checkout(items, coupons)
+  cart = consolidate_cart(items)
+  cart1 = apply_coupons(cart, coupons)
+  cart2 = apply_clearance(cart1)
+
+  total = 0
+
+  cart2.each do |name, price_hash|
+    total += price_hash[:price] * price_hash[:count]
+  end
+
+  total > 100 ? total * 0.9 : total
+
 end
--- random_grocery_generator.rb
+++ random_grocery_generator.rb
@@ -1,8 +1,8 @@
 require_relative 'grocer'
 
 def items
-	[
-		{"AVOCADO" => {:price => 3.00, :clearance => true}},
+	[	
+	  {"AVOCADO" => {:price => 3.00, :clearance => true}},
 		{"KALE" => {:price => 3.00, :clearance => false}},
 		{"BLACK_BEANS" => {:price => 2.50, :clearance => false}},
 		{"ALMONDS" => {:price => 9.00, :clearance => false}},
