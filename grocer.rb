64789cc31806dc73e940c6510af4209715408fb5
Dante Spencer <spencerdantel@gmail.com>
2019-07-11 12:24:21 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,26 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+  # cart = [{}]
+  hash = {}
+  i = 0
+  while i < cart.length do
+    # if hash includes item
+  item = cart[i]
+  key = item.keys[0]
+    if hash.has_key?(key)
+# increment the key's value's quantity attribute. 
+       i += 1     
+    else
+# write in a new key/value pair. The value is a hash with price, quantity, truthy/falsey
+     cart.select( |i| i=i )
+      
+     i += 1   
+     
+
+    end
+  end
+  hash
 end
 
 def apply_coupons(cart, coupons)
