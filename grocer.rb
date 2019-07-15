f8a3ddb7bd02a224442e838b1da94d3318e50c66
Michael Martens <omichaelaaron@gmail.com>
2019-07-15 14:47:36 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -43,9 +43,26 @@ def apply_coupons(cart, coupons)
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |item_name, item_data|
+    if item_data[:clearance]
+      item_data[:price] = (item_data[:price] * 0.8).round(1)
+    end
+  end
 end
 
 def checkout(cart, coupons)
-  # code here
+  cart = consolidate_cart(cart: cart)
+end 	
+   cart = apply_coupons(cart: cart, coupons: coupons)
+  cart = apply_clearance(cart: cart)
+  total = 0
+  cart.each do |item_name, item_data|
+    total += (item_data[:price] * item_data[:count])
+  end
+
+   if total > 100
+    0.9 * total
+  else
+    total
+  end
 end
