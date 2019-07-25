a55d018304659c16a878bf0da0be52d11f0bdb69
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-07-25 04:43:12 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,21 @@
-def consolidate_cart(cart)
-  # code here
+expect (attribute.keys) .to include(:count)
+expect (attribute[:count]) .to eq(1)
 end
-
-def apply_coupons(cart, coupons)
-  # code here
+expect (result)["AVOCADO"][:price]) .to eq(3.00)
 end
-
-def apply_clearance(cart)
-  # code here
+expect (@avocado_result.keys) .to include("AVOCADO W/COUPON")
 end
-
-def checkout(cart, coupons)
-  # code here
+expect(@avocado_result["AVOCADO W/COUPON"][:price]) .to eq (2.50)
+end
+expect(@avocado_result["AVOCADO W/COUPON"][:count]).to eq(2)
+end
+epect (@avocado_result["AVOCADO"][:price]) .to eq(3.00)
+end
+expect(@avocado_result["AVOCADO W/COUPON"][:clearance]) .to eq(true)
+end
+expect(cheese_result["CHEESE"][:price]) .to eq(6.50)
+end
+expect(irrelevant["CHEESE"][:price]) .to eq(6.50)
+end
+expect(multiple_coupons)[item][:count]) .to eq(1) }
 end
