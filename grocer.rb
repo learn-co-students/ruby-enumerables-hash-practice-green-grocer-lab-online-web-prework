1f8d12ab6da83e99a93af4303ccafdc9e7b60d72
Esther Agyare-Boateng <eagyare91@gmail.com>
2019-08-01 22:20:34 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,24 @@
 def consolidate_cart(cart)
-  # code here
+def consolidate_cart(item)
+  final = Hash.new 0 
+  count = :count
+item.each do |hash|
+  hash.each do |food, description|
+  
+if final.has_key?(food) == false
+  final[food] = description
+  final[food][count] = 1
+elsif final.has_key?(food)
+final[food][:count] +=1
+end
+final
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  final = Hash.new 0 
+  coupons.each do |key, value|
+puts key
+cart.each do |food, description|
 end
 
 def apply_clearance(cart)
