07ea56b9c7a0ba570791d87c1f01a98cea3dc907
Srikalyan Megati <kalmegati@gmail.com>
2019-08-03 21:23:40 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,33 @@
-def consolidate_cart(cart)
-  # code here
-end
+#def consolidate_cart(cart)
+#  windfall = []
+#  cart.each {|item| windfall.push([(item.keys)[0], item[(item.keys)[0]]])}
+#  p windfall
+#  organized_cart = {}
+#  windfall.length.times { |index|
+#    cart_key = windfall[index]
+#    if organized_cart.keys.include? windfall[index] 
+#      organized_cart[cart_key][:count] += 1
+#    else
+#      organized_cart[cart_key] => windfall[index]
+#      organized_cart[cart_key][:count] => 0
+#    end
+#  }
+#end
+
+def
+
+#def consolidate_cart(cart)
+#  overcart = {} #creates the hash that the method will return
+#  cart.length.times { |index| #asks to iterate over every hash in the cart array
+#    if overcart.key?(cart[index].keys[0]) # if the product already exists in overcart...
+#      overcart[cart[index].keys[0]][:count] += 1 #...increase the count of the product by one
+#    else #if overcart does not yet contain the product...
+#      overcart[cart[index].keys[0]] = cart[index].keys[0]
+#      overcart[cart[index].keys[0]][:count] = 0
+#    end
+#  }
+#  puts overcart
+#end
 
 def apply_coupons(cart, coupons)
   # code here
@@ -13,3 +40,11 @@ end
 def checkout(cart, coupons)
   # code here
 end
+
+craving = [
+  {"AVOCADO" => {:price => 3.00, :clearance => true }},
+  {"AVOCADO" => {:price => 3.00, :clearance => true }},
+  {"KALE"    => {:price => 3.00, :clearance => false}}
+]
+
+consolidate_cart(craving)
