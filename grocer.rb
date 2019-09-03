e5aa289fe3ac6574f5741dc49dac282e76f421ad
Sarah H <sarahrose.henderson@gmail.com>
2019-09-03 19:54:04 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,36 @@
+
 def consolidate_cart(cart)
   # code here
+final_hash = {}
+cart.each do |element_hash|
+  element_name = element_hash.keys[0]
+
+  if final_hash.has_key?(element_name)
+    final_hash[element_name][:count] += 1
+  else
+    final_hash[element_name] = {
+count: 1,
+price: element_hash[element_name][:price],
+clearance: element_hash[element_name][:clearance]
+    }
+end
+end
+final_hash
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  coupons.each do |coupon|
+    item = coupon[:item]
+    if cart[item] && coupon[:item][:count] >= coupon[:num] &&cart["#{item} W/COUPON"]
+      cart["#{item} W/COUPON"] = {price: coupon[:cost] / coupon[:num] , clearance: cart[item][:clearance], count: coupon[:num]}
+cart[item][:count] -= coupon[:num]
+elseif coupon[:item][:count] >= coupon[:num] &&cart["#{item} W/COUPON"]
+cart["#{item} W/COUPON"][:count] += coupton[:num]
+cart[item][:count] -= coupon[:num]
+end
+  end
+  cart
 end
 
 def apply_clearance(cart)
