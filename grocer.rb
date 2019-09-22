242cb42a8a12b1531b35c4c77791c73bf7835e8a
Matthew Jones <matts3371@gmail.com>
2019-09-22 23:58:24 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,82 @@
-def consolidate_cart(cart)
-  # code here
+def consolidate_cart(cart) 
+  new_cart = {} 
+  cart.each do |items_array| 
+    items_array.each do |item, attribute_hash| 
+      new_cart[item] ||= attribute_hash 
+      new_cart[item][:count] ? new_cart[item][:count] += 1 :   
+      new_cart[item][:count] = 1 
+  end 
+end 
+new_cart 
 end
 
-def apply_coupons(cart, coupons)
-  # code here
+def apply_coupons(cart, coupons) 
+  
+  coupons.each do |coupon| 
+    coupon.each do |attribute, value| 
+      name = coupon[:item] 
+      
+      if cart[name] && cart[name][:count] >= coupon[:num] 
+        if cart["#{name} W/COUPON"]
+          cart["#{name} W/COUPON"][:count] += coupon[:num]
+        else 
+          cart["#{name} W/COUPON"] = {:price => coupon[:cost] / coupon[:num], 
+          :clearance => cart[name][:clearance], :count => coupon[:num]} 
+        end 
+  
+      cart[name][:count] -= coupon[:num] 
+    end 
+  end 
+end 
+  cart 
 end
 
-def apply_clearance(cart)
-  # code here
+def apply_clearance(cart) 
+  cart.each do |item, attribute_hash| 
+    if attribute_hash[:clearance] == true 
+      attribute_hash[:price] = (attribute_hash[:price] *
+      0.8).round(2) 
+    end 
+  end 
+cart 
 end
 
-def checkout(cart, coupons)
-  # code here
+def checkout(cart, coupons) 
+  total = 0 
+  new_cart = consolidate_cart(cart) 
+  coupon_cart = apply_coupons(new_cart, coupons) 
+  clearance_cart = apply_clearance(coupon_cart) 
+  clearance_cart.each do |item, attribute_hash| 
+    total += (attribute_hash[:price] * attribute_hash[:count])
+  end 
+if total > 100
+  total = (total * 0.9)
 end
+total
+end
+
+items =   [
+      {"AVOCADO" => {:price => 3.00, :clearance => true}},
+      {"AVOCADO" => {:price => 3.00, :clearance => true}},
+      {"AVOCADO" => {:price => 3.00, :clearance => true}},
+      {"AVOCADO" => {:price => 3.00, :clearance => true}},
+      {"AVOCADO" => {:price => 3.00, :clearance => true}},
+      {"KALE" => {:price => 3.00, :clearance => false}},
+      {"BLACK_BEANS" => {:price => 2.50, :clearance => false}},
+      {"ALMONDS" => {:price => 9.00, :clearance => false}},
+      {"TEMPEH" => {:price => 3.00, :clearance => true}},
+      {"CHEESE" => {:price => 6.50, :clearance => false}},
+      {"BEER" => {:price => 13.00, :clearance => false}},
+      {"PEANUTBUTTER" => {:price => 3.00, :clearance => true}},
+      {"BEETS" => {:price => 2.50, :clearance => false}},
+      {"SOY MILK" => {:price => 4.50, :clearance => true}}
+    ]
+
+coupons = [
+      {:item => "AVOCADO", :num => 2, :cost => 5.00},
+      {:item => "AVOCADO", :num => 2, :cost => 5.00},
+      {:item => "BEER", :num => 2, :cost => 20.00},
+      {:item => "CHEESE", :num => 2, :cost => 15.00}
+    ]
+
+checkout(items, coupons)
