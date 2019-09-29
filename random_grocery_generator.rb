af16ddd28a2d23c5bdc4e5fcdbcfa8c0957fb9a1
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-09-29 15:08:06 +0000

    Done.

--- random_grocery_generator.rb
+++ random_grocery_generator.rb
@@ -1,50 +1,57 @@
-def consolidate_cart(cart)
-  final_hash = {}
-  cart.each do |element_hash|
-   element_name = element_hash.keys[0]
-   
-     if final_hash.has_key?(element_name)
-       final_hash[element_name][:count] += 1 
-     else
-      final_hash[element_name] = {
-      count: 1,
-      price: element_hash[element_name][:price],
-      clearance: element_hash[element_name][:clearance]
-     }
-     end
-  end
- final_hash
- end
+require_relative 'grocer'
 
-def apply_coupons(cart, coupons)
- coupons.each do |coupon|
-   item = coupon[:item]
-   if cart.has_key?[item]
-     if cart[:item][:count] >= coupon[:num]
-       if cart[item]  && !cart.has_key?["#{item} W/COUPON"]
-          cart["#{item} W/COUPON"] = {price: coupon[:cost] / coupon[:num], clearance: cart[item][clearance:], count: coupon[:num] }
-       else
-           cart["#{item} W/COUPON"][:count] += coupon[:num]
-       end
-       cart[item][:count] -= coupon[:num]
-     end
-    end
-   end
-   cart
- end
-
-def apply_clearance(cart)
- cart.each do |product_name, stats|
-   stats[:price] -= stats[:price] * 0.2 if stats[:clearance]
-end
-cart
+def items
+	[
+		{"AVOCADO" => {:price => 3.00, :clearance => true}},
+		{"KALE" => {:price => 3.00, :clearance => false}},
+		{"BLACK_BEANS" => {:price => 2.50, :clearance => false}},
+		{"ALMONDS" => {:price => 9.00, :clearance => false}},
+		{"TEMPEH" => {:price => 3.00, :clearance => true}},
+		{"CHEESE" => {:price => 6.50, :clearance => false}},
+		{"BEER" => {:price => 13.00, :clearance => false}},
+		{"PEANUTBUTTER" => {:price => 3.00, :clearance => true}},
+		{"BEETS" => {:price => 2.50, :clearance => false}}
+	]
 end
 
-def checkout(array, coupons)
-  hash_cart = consolidate_cart(array)
-  applied_coupons = apply_coupons(hash_cart, coupons)
-  applied_discount = apply_clearance(applied_coupons)
- total = applied_discount.reduce(0) { |acc, (key, value)| acc += value[:price] * value[:count] }
- total > 100 ? total * 0.9 : total
+def coupons
+	[
+		{:item => "AVOCADO", :num => 2, :cost => 5.00},
+		{:item => "BEER", :num => 2, :cost => 20.00},
+		{:item => "CHEESE", :num => 3, :cost => 15.00}
+	]
 end
 
+def generate_cart
+	[].tap do |cart|
+		rand(20).times do
+			cart.push(items.sample)
+		end
+	end
+end
+
+def generate_coupons
+	[].tap do |c|
+		rand(2).times do
+			c.push(coupons.sample)
+		end
+	end
+end
+
+cart = generate_cart
+coupons = generate_coupons
+
+puts "Items in cart"
+cart.each do |item|
+	puts "Item: #{item.keys.first}"
+	puts "Price: #{item[item.keys.first][:price]}"
+	puts "Clearance: #{item[item.keys.first][:clearance]}"
+	puts "=" * 10
+end
+
+puts "Coupons on hand"
+coupons.each do |coupon|
+	puts "Get #{coupon[:item].capitalize} for #{coupon[:cost]} when you by #{coupon[:num]}"
+end
+
+puts "Your total is #{checkout(cart: cart, coupons: coupons)}"
