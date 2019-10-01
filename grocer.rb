56f21719a032e5f71f2c0e8b40d5a914e711d61b
Jordan Long <jordanleelong5@gmail.com>
2019-10-01 18:22:30 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -30,10 +30,10 @@ def apply_coupons(cart, coupons)
         cart[item][:count] -= coupon[:num]
       end
         end
-          return cart
       end
-
+      return cart
 end
+
 
 
 def apply_clearance(cart)
@@ -47,6 +47,6 @@ def checkout(array, coupons)
   hash_cart = consolidate_cart(array)
   applied_coupons = apply_coupons(hash_cart, coupons)
   applied_discount = apply_clearance(applied_coupons)
-  total = applied_discount.reduce(0) { |acc, key, value| acc += value[:price] * value[:count] }
+  total = applied_discount.reduce(0) { |acc, (key, value)| acc += value[:price] * value[:count] }
   total > 100 ? total * 0.9 : total
 end
