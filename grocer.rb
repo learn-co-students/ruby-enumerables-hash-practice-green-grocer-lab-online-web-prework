0ad0d8368ff9984a889c9f7f16ab3d6779401d15
Laura Hatch <laura.hatch.lh@gmail.com>
2019-10-05 01:59:30 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -22,7 +22,8 @@ def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     item = coupon [:item]
   if cart[item]&& coupon[:item][:count] >= coupon[:num] && !cart.has_key?["#{item}W/COUPON"]
-    cart ["#{item} W/COUPON"] = {price:, cleasrence:,count:}
+    cart ["#{item} W/COUPON"] = {price:coupon[: cost] W/COUPON [:num] ,
+      clearence: cart [item][:clearence] ,count:}
   if cart.has_key?(item)
     if cart[item][:count] >= coupon [:num]
       if !cart[coupon_item]
